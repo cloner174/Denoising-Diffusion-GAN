d4caf697d6812b5ae47a7fa7219f9024944c99f1
cloner174 <hamedhajipour1400@gmail.com>
2024-10-17 07:29:39 +0330

    add option for 3d images

--- additionals/images.py
+++ additionals/images.py
@@ -148,6 +148,127 @@ def nii_to_png(slices_info, save_dir = './real_images', only_z = True, lim = Non
 
 
 
+def nii_to_npy_simple(nii_file_path, 
+                      _where_, 
+                      slice_index, 
+                      only_z = True,
+                      save_dir = './real_images', 
+                      transform = None):
+    try:
+        import nibabel as nib
+    except ModuleNotFoundError:
+        raise ModuleNotFoundError("Please Install NiBabel Lib using: pip install nibabel")
+    
+    patch = nib.load(nii_file_path).get_fdata()
+    
+    if slice_index < 0 or slice_index >= 256:
+        raise IndexError(f"Slice index {slice_index} out of bounds for patch with shape {patch.shape}")
+    
+    if only_z:
+        if _where_ == 'z':
+            image_2d = patch[ : , : ,  slice_index ]
+        else:   
+            return
+    else:
+        if _where_ == 'x':
+            image_2d = patch[ slice_index , : , : ]
+        elif _where_ == 'y':
+            image_2d = patch[ : , slice_index, : ]
+        elif _where_ == 'z':
+            image_2d = patch[ : , : , slice_index ]
+    
+    image_2d = Image.fromarray(image_2d.astype(np.uint8))
+    if transform is not None:
+        try:
+            image_2d = image_2d.reshape(transform)
+        except:
+            pass
+    
+    temp_name = os.path.split(nii_file_path)[-1].split('.nii.gz')[0]
+    temp_name += f'_{_where_}_{slice_index}.npy'
+    np.save(os.path.join(save_dir, temp_name), image_2d)
+    return
+
+
+
+def nii_to_npy_3d(slices_info, bounders, _where_, save_dir = './real_numpy_3d'):
+    
+    try:
+        import nibabel as nib
+    except ModuleNotFoundError:
+        raise ModuleNotFoundError("Please Install NiBabel Lib using: pip install nibabel")
+    
+    os.makedirs(save_dir, exist_ok=True)
+    patch_out = []
+    patch_name = []
+    to_add = 0
+    for i in range( len(slices_info) - 1 ):
+        i += to_add
+        j = i + 1
+        if i > len(slices_info) or j > len(slices_info):
+            break
+        temp_slice = []
+        temp_name = slices_info[i][0]
+        patch_in = nib.load(slices_info[i][0]).get_fdata()
+        while slices_info[i][0] == slices_info[j][0]:
+            if len(temp_slice) == 0 and slices_info[i][1] == _where_:
+                temp_slice.append(slices_info[i][-1])
+            if slices_info[j][1] == _where_ :
+                temp_slice.append(slices_info[j][-1])
+            to_add = j
+            j += 1
+        while len(temp_slice) >= bounders + 1 :
+            temp_slices_new = temp_slice[:bounders+1]
+            if _where_ == 'x':
+                image_3d = patch_in[ temp_slices_new[0] : temp_slices_new[-1] , : , : ]
+            elif _where_ == 'y':
+                image_3d = patch_in[ : , temp_slices_new[0] : temp_slices_new[-1] , : ]
+            elif _where_ == 'z':
+                image_3d = patch_in[ : , : , temp_slices_new[0] : temp_slices_new[-1] ]
+            patch_out.append( image_3d )
+            patch_name.append( temp_name )
+            for _ in range(bounders):
+                temp_slice.pop(0)
+    
+    for i in range(len(patch_out)):
+        name_ = os.path.split(patch_name[i])[-1].split('.nii.gz')[0] 
+        name_ += f'_{_where_}_{i}.npy'
+        np.save(os.path.join(save_dir, name_), patch_out[i])
+    
+    return 
+
+
+def nii_to_npy(slices_info, 
+               save_dir = './real_numpy', 
+               only_z = True, 
+               lim = None, 
+               do_transform_for = 'none'):#can be 'train' or 'val' or 'none'
+    
+    os.makedirs(save_dir, exist_ok=True)
+    
+    if do_transform_for == 'train':
+        transform, _ = _data_transforms_luna16()
+    elif do_transform_for == 'val':
+        _, transform = _data_transforms_luna16()
+    else:
+        transform = None
+    if lim is not None:
+            lim = lim if isinstance(lim, int) else 1000
+    
+    for any_ in slices_info :
+        if lim is not None:
+            if len( os.listdir(save_dir) ) > lim:
+                return
+        nii_file_path, _where_, slice = any_
+        nii_to_npy_simple(nii_file_path, _where_, slice, only_z, save_dir , transform )
+    
+    return
+
+
+
+
+
+
 
 if __name__ == '__main__':
     
--- datasets_prep/custom.py
+++ datasets_prep/custom.py
@@ -16,25 +16,51 @@ from additionals.utilities import load_slice_info, save_slice_info
 
 class Luna16Dataset(Dataset):
     
-    def __init__(self, data_dir, mask_dir, transform=None, bound_exp_lim = 5, path_to_slices_info = None):
+    def __init__(self, 
+                 data_dir, 
+                 mask_dir = None,
+                 transform=None, 
+                 bound_exp_lim = 5, 
+                 
+                 _3d:bool = False, # if True, the return items will be in shape -> (img_size, img_size, bounds)
+                 bounders: None|int = None,# Only works when _3d is True:: if not None, Should be an integer : and class will generates 3d_images
+                 
+                 single_axis:bool = True, # when it's False, all three dims will be generates and converts!
+                 _where: str|None = None,# 'z' or 'x' or 'y' ? #  only works when single_axis == True | if None and single_axis is true, defualts will be used!
+                 
+                 path_to_slices_info = None):
         
         self.transform = transform
         self.data_dir = data_dir
         self.mask_dir = mask_dir
         self.bound_exp_lim = bound_exp_lim
         
+        self._3d = _3d
+        self._3d_slices_info = [] if self._3d else None
+        self.bounders = bounders
+        self.single_axis = single_axis if single_axis is not None else 'z'
+        self._where_ = _where
+        
         if path_to_slices_info is not None:
             self.path_to_slice_info = path_to_slices_info
             self.slice_info = load_slice_info(path_to_slices_info)
+        
         else:
             self.slice_info = []  # List of tuples: ( .nii.gz file path, slice index)
             self._prepare_dataset()
             save_slice_info(self.slice_info)
+        
+        if self._3d:
+            self.__get_bounds__()
+    
     
     def _prepare_dataset(self):
         if not os.path.isdir(self.data_dir):
             raise FileNotFoundError(f"Please check your data_dir path and try again. The current path is: {self.data_dir}")
         # Get all .nii.gz files
+        if self.mask_dir == None:
+            raise FileNotFoundError(f"Please check your mask_dir path and try again. The current path is: {self.mask_dir}")
+        
         nii_files =  [os.path.join(self.data_dir, file) for file in os.listdir(self.data_dir) if file.endswith(".nii.gz")]
         if not nii_files:
             raise FileNotFoundError("No patches found in the specified directory.")
@@ -50,14 +76,17 @@ class Luna16Dataset(Dataset):
             else:
                 Dx, Dy, Dz = handled_indexes
             
-            for i in range(len(Dx)):
-                self.slice_info.append((nii_file_path, 'x', Dx[i]))
+            if self.single_axis and self._where_ == 'x' or not self.single_axis:
+                for i in range(len(Dx)):
+                    self.slice_info.append((nii_file_path, 'x', Dx[i]))
             
-            for i in range(len(Dy)):
-                self.slice_info.append((nii_file_path, 'y', Dy[i]))
+            if self.single_axis and self._where_ == 'y' or not self.single_axis:
+                for i in range(len(Dy)):
+                    self.slice_info.append((nii_file_path, 'y', Dy[i]))
             
-            for i in range(len(Dz)):
-                self.slice_info.append((nii_file_path, 'z', Dz[i]))
+            if self.single_axis and self._where_ == 'z' or not self.single_axis:
+                for i in range(len(Dz)):
+                    self.slice_info.append((nii_file_path, 'z', Dz[i]))
         
     
     def __handle_edges__(self, indxes):
@@ -89,35 +118,87 @@ class Luna16Dataset(Dataset):
         if max_bound_z + self.bound_exp_lim < data_shape[2] :
             max_bound_z += self.bound_exp_lim
         
-        Dx = range(min_bound_x,max_bound_x)
-        Dy = range(min_bound_y,max_bound_y)
-        Dz = range(min_bound_z,max_bound_z)
+        Dx = range(min_bound_x,max_bound_x) if not self._3d else range(min_bound_x,max_bound_x, self.bounders)
+        Dy = range(min_bound_y,max_bound_y) if not self._3d else range(min_bound_y,max_bound_y, self.bounders)
+        Dz = range(min_bound_z,max_bound_z) if not self._3d else range(min_bound_z,max_bound_z, self.bounders)
         return Dx, Dy, Dz
     
     def __len__(self):
         return len(self.slice_info)
     
+    def  __get_bounds__(self):
+        if self.single_axis:
+            _where_all = [ self._where_]
+        else:
+            _where_all = [ 'x', 'y', 'x']
+        
+        for _where_ in _where_all:
+            to_add = 0
+            for i in range( len(self.slice_info ) - 1 ):
+                i += to_add
+                j = i + 1
+                if i > len(self.slice_info ) or j > len(self.slice_info ):
+                    break
+                temp_slice = []
+                temp_name = self.slice_info[i][0]
+                patch_in = nib.load(self.slice_info[i][0]).get_fdata()
+                while self.slice_info[i][0] == self.slice_info[j][0]:
+                    if len(temp_slice) == 0 and self.slice_info[i][1] == _where_:
+                        temp_slice.append(self.slice_info[i][-1])
+                    
+                    if self.slice_info[j][1] == _where_:
+                        temp_slice.append(self.slice_info[j][-1])                    
+                    
+                    to_add = j
+                    j += 1
+                
+                while len(temp_slice) >= self.bounders + 1 :
+                    temp_slices_new = temp_slice[:self.bounders+1]
+                    
+                    self._3d_slices_info.append( temp_name , _where_, temp_slices_new )
+                    
+                    for _ in range(self.bounders):
+                        temp_slice.pop(0)
+    
+    
     def __getitem__(self, index):
-        nii_file_path, _where_, slice_index = self.slice_info[index]
-        patch = nib.load(nii_file_path).get_fdata()
+
         # Get the specified slice
-        if slice_index < 0 or slice_index >= 256:
-            raise IndexError(f"Slice index {slice_index} out of bounds for patch with shape {patch.shape}")
+        if self._3d:
+            nii_file_path, _where_, slice_index = self._3d_slices_info[index]
+            patch = nib.load(nii_file_path).get_fdata()
+            if slice_index[0] < 0 or slice_index[0] >= 256 or  slice_index[-1] < 0 or slice_index[-1] >= 256 :
+                raise IndexError(f"Slice index {slice_index} out of bounds for patch with shape {patch.shape}")
+            if _where_ == 'x':
+                image_3d = patch[ slice_index[0] : slice_index[-1] , : , : ]
+            elif _where_ == 'y':
+                image_3d = patch[ : , slice_index[0] : slice_index[-1] , : ]
+            elif _where_ == 'z':
+                image_3d = patch[ : , : , slice_index[0] : slice_index[-1] ]
+            img = image_3d
+        else:
+            nii_file_path, _where_, slice_index = self.slice_info[index]
+            patch = nib.load(nii_file_path).get_fdata()
+            if slice_index < 0 or slice_index >= 256:
+                raise IndexError(f"Slice index {slice_index} out of bounds for patch with shape {patch.shape}")
         
-        if _where_ == 'x':
-            image_2d = patch[ slice_index , : , : ]
+            if _where_ == 'x' and _where_ in self._where_:
+                image_2d = patch[ slice_index , : , : ]
         
-        elif _where_ == 'y':
-            image_2d = patch[ : , slice_index, : ]
+            elif _where_ == 'y' and _where_ in self._where_:
+                image_2d = patch[ : , slice_index, : ]
         
-        elif _where_ == 'z':
-            image_2d = patch[ : , : , slice_index ]
+            elif _where_ == 'z' and _where_ in self._where_:
+                image_2d = patch[ : , : , slice_index ]
+            
+            img = image_2d
         
-        image_2d = Image.fromarray(image_2d.astype(np.uint8))
+        img = Image.fromarray(img.astype(np.uint8))
         if self.transform is not None:
-            image_2d = self.transform(image_2d)
+            img = self.transform(img)
         
-        return image_2d, 1  # 'Dummy' label! برای مدل جن نیاز ی به لیبل نیست
+        return img, 1  # 'Dummy' label! برای مدل جن نیاز ی به لیبل نیست
+
 
 
 
